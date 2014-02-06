adafa839780a061e9935e09f4a6ecebc663fca4d
tomsbergmanis <tomsbergmanis@gmail.com>
2014-02-06 01:30:13 +0000

    added append to voc class

--- vocobulary.py
+++ vocobulary.py
@@ -7,16 +7,18 @@ class voc(object):
         lines = file_content.split("\n")
         voc = {}
         inv_voc = {}
-        i = 1
+        i = 0
         for line in lines:
+            i += 1
             if line == "":
                 continue
             element = line.split(" ")
             var_name = string+str(i)
             voc[element[0]] = var_name
             inv_voc[var_name] = element[0]
-            i += 1
             
+        self.i = i
+        self.string = string
         self.voc = voc
         self.inv_voc = inv_voc
         
@@ -29,3 +31,28 @@ class voc(object):
                 return self.inv_voc[word]
             except KeyError, e:
                 return ""
+                
+    def append(slef, ifile,  string):
+        
+        with open(ifile, 'r') as f:
+            file_content = f.read()
+            
+        lines = file_content.split("\n")
+        voc = self.voc
+        inv_voc = self.inv_voc
+        i = self.i
+        for line in lines:
+            i += 1
+            if line == "":
+                continue
+            element = line.split(" ")
+            if element[0] in voc:
+                continue
+            var_name = string+str(i)
+            voc[element[0]] = var_name
+            inv_voc[var_name] = element[0]
+            
+        self.i = i
+        self.voc = voc
+        self.inv_voc = inv_voc
+    
