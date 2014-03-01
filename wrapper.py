43e66e5100a34bf3d717642f63d8fe11e4b089c1
tomsbergmanis <tomsbergmanis@gmail.com>
2014-03-01 12:38:42 +0000

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,7 @@
+import sys
 import distributions as D
 import vocobulary as V
-import gzip as G
+import os
 
 # a = w.wrapper("../data/en_unigrams", "../data/lv_unigrams", "../data/lex.e2f", "../data/wiki_eng_unigrams","../data/wiki_lv_unigrams", "t", "s" )
 
@@ -11,13 +12,15 @@ class wrapper(object):
         self.MD_S = D.word_probability(wiki_s_file)
         self.MD_T = D.word_probability(wiki_t_file)
         
-        self.V_S = V.voc(corp_s_voc_file, s_str)
-        self.V_S.append(wiki_s_file)
+        self.V_S = V.voc(wiki_s_file, s_str)
+       # self.V_S.append(wiki_s_file)
         
-        self.V_T = V.voc(corp_t_voc_file, t_str)
-        self.V_T.append(wiki_t_file)
+        self.V_T = V.voc(wiki_t_file, t_str)
+        self.s_str = s_str
+        self.t_str = t_str
+       # self.V_T.append(wiki_t_file)
         self.out_file = out_file
-      
+    
     
     def create_objective(self):
         s_vs = self.V_S.voc_items()
@@ -45,8 +48,10 @@ class wrapper(object):
                     o.write(c2)
                     o.write(c3)
                     c4 += sv + tv + "+"
+                o.flush()
             c4 += "0=1;\n"
             o.write(c4)
+
                 
     def create_s_m(self):
         s_vs = self.V_S.voc_items()
@@ -58,8 +63,9 @@ class wrapper(object):
                     a += sv + tv + "+"
                 a += "0 =" + str(self.MD_S.getter(sw))+";"
                 o.write(a+"\n")
+                o.flush()
 
-                
+
     def create_t_m(self):
         s_vs = self.V_S.voc_items()
         t_vs = self.V_T.voc_items()
@@ -68,15 +74,44 @@ class wrapper(object):
                 a = ""
                 for sw, sv in self.V_S.voc_items():
                     a += sv + tv + "+"
-                a += "0 =" + str(self.MD_T.getter(sw))+";"
+                a += "0 =" + str(self.MD_T.getter(tw))+";"
                 o.write(a+"\n")
+                o.flush()
 
+    def unwrap(self, wfile):
+        f.open(wfile, "r")
+        fst = f.read()
+        vlist = fst.split("\n")
+        for vl in vlist:
+            vel = vl.split(" ")
+            vn = vel[0]
+            if not vn[0] ==  'x':
+                continue
+            else:
+                print vel 
+                words = self.get_words(vn[1:])
+                
+                print word[0] + " " + word[1] + " " + vel[-1]
+                
+             
+    def get_words(self,s):         #xs68t10
+       varl = s.split(self.t_str)
+       tvar = self.t_str + varl[-1]
+       svar = varl[0][1:]
+       tword = self.V_T.getter(tvar)
+       sword = self.V_S.getter(svar)
+       return (tword, sword)
+      
             
 if __name__ == "__main__":
-    a = wrapper("../data/en_unigrams", "../data/lv_unigrams", "../data/lex.e2f", "../data/wiki_eng_unigrams","../data/wiki_lv_unigrams", "t", "s", "out" )
+    print "Wrpapping..."
+    a = wrapper("/home/toms/lp/data/en_unigrams", "/home/toms/lp/data/lv_unigrams", "/home/toms/lp/data/lex.e2f", sys.argv[1], sys.argv[2], "t", "s", sys.argv[3])
     a.create_objective()
     a.create_s_m()
     a.create_t_m()
     a.create_of_c()
-    
-    
+    print "Wrpapping is done."
+    print "Solving LP..."
+    os.system(".././lp_solve " + sys.argv[3] + " " + sys.argv[3])
+    print "Solving LP..."
+    a.unwarp(sys.argv[3])
